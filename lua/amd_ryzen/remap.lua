7426d38b50cfd087ad888be8d2d3a3dab03350c3
Arnur <arca26100@gmail.com>
2025-07-30 10:25:58 +0500

    feat: add harpoon and supermaven

--- lua/amd_ryzen/remap.lua
+++ lua/amd_ryzen/remap.lua
@@ -26,8 +26,8 @@ vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<M-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<M-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
@@ -39,3 +39,4 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
